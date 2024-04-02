36d2feb3cd8b271f2a3ab9f35e1f1aa1d289079e
Yuvrajbh <yuvrajbhalla05@gmail.com>
2024-04-02 12:26:56 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get("", { withCredentials: true })
+        const response = await axios.get("http://localhost:4000/api/v1/user/getuser", { withCredentials: true })
         setIsAuthorized(true)
         setUser(response.data.user)
       } catch (error) {
--- Frontend/src/components/Layout/Navbar.jsx
+++ Frontend/src/components/Layout/Navbar.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { Context } from '../../main'
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import axios from 'axios';
 
 const Navbar = () => {
+  const [isAuthorized,setIsAuthorized,user,setUser]=useContext(Context)
+  const [show,setShow]=useState(false);
+
+  const navigateTo=useNavigate()
+  const handleLogout=async()=>{
+    try {
+      const response=await axios.get("http://localhost:4000/api/v1/user/logout",{withCredentials:true})
+      setIsAuthorized(false)
+      toast.success(response.data.message)
+      navigateTo('/login')
+    } catch (error) {
+      toast.error(error.response.data.message)
+      setIsAuthorized(true)
+    }
+  }
   return (
     <div>
       
